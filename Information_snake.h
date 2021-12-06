f52818d7b8cc183d38c834c66a5b1494abb6fc18
Alla Yukhnevych <allauhnevich25@gmail.com>
2021-12-06 23:27:49 +0200

    Create class Snake

---
+++ Information_snake.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ManagerTexture.h"
+
+class Snake {
+public:
+    int snake_position_x = field_size_x / 2;
+    int snake_position_y = field_size_y / 2;
+
+    int snake_lenght = 4;
+
+    int snake_direction = SNAKE_DIRECTION_RIGHT;
+    int field[field_size_y][field_size_x];
+};
