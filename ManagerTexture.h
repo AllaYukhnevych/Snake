ae694fb4ec022e7b4e7b95feb28490aa5ac149f8
Alla Yukhnevych <allauhnevich25@gmail.com>
2021-12-06 23:28:26 +0200

    Load media

---
+++ ManagerTexture.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "LTexture.h"
+
+LTexture none_texture;
+LTexture snake_texture;
+LTexture apple_texture;
+LTexture wall_texture;
+
+bool loadMedia()
+{
+    bool success = true;
+
+    if (!none_texture.loadFromFile("wp2409705.png"))
+    {
+        printf("Failed to load arrow texture!\n");
+        success = false;
+    }
+    if (!snake_texture.loadFromFile("snake.png"))
+    {
+        printf("Failed to load arrow texture!\n");
+        success = false;
+    }
+    if (!apple_texture.loadFromFile("apple.png"))
+    {
+        printf("Failed to load arrow texture!\n");
+        success = false;
+    }
+    if (!wall_texture.loadFromFile("wall.png"))
+    {
+        printf("Failed to load arrow texture!\n");
+        success = false;
+    }
+    return success;
+}
